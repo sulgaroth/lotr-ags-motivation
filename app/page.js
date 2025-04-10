aad2eba04a582f09373cbbedf1578e208ecea0d5
mehmetbardakci <mbardakci1040@gmail.com>
2025-04-11 01:09:34 +0300

    dosyalar yüklendi

--- app/page.js
+++ app/page.js
@@ -1,102 +1,96 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import { motivasyonCumleleri } from "./data/motivation";
+import Link from "next/link";
+import { SiApplemusic } from "react-icons/si";
+import { styled } from "@mui/material/styles";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [index, setIndex] = useState(0);
+  const [fade, setFade] = useState(true);
+  const [audio, setAudio] = useState(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setAudio(new Audio("/lotr.mp3")); // Audio nesnesini sadece istemci tarafında oluştur
+    }
+  }, []);
+
+  const ToolTip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: theme.palette.common.white,
+      color: "rgba(0, 0, 0, 0.87)",
+      boxShadow: theme.shadows[1],
+      fontSize: 17,
+    },
+  }));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false); // Önce fade-out
+      setTimeout(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % motivasyonCumleleri.length);
+        setFade(true); // Sonra fade-in
+      }, 500); // Fade-out süresi
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    // Bileşen unmount olduğunda müziği durdur
+    return () => {
+      if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    };
+  }, [audio]);
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center relative">
+      <p
+        className={`transition-opacity duration-500 text-2xl sm:text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold ${
+          fade ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        {motivasyonCumleleri[index]}
+      </p>
+
+      <footer className="absolute bottom-4 w-full flex flex-col sm:flex-row flex-wrap justify-center items-center text-sm text-gray-500 px-4 gap-2">
+        <div className="w-full sm:w-auto text-center">
+          {currentYear} |{" "}
+          <Link
+            className="underline font-semibold hover:opacity-70 duration-300"
+            href={"https://github.com/sulgaroth"}
+            target={"_blank"}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Sulgaroth
+          </Link>{" "}
+          tarafından geliştirildi.
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <ToolTip
+          title="Kulak ver, Lothlórien'in yapraklarının arasında yankılanan eski melodilere."
+          className="flex justify-center items-center h-fit mt-2 sm:mt-0"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          <button
+            className="text-white flex items-center justify-center cursor-pointer"
+            onClick={() => {
+              if (audio.paused) {
+                audio.play();
+              } else {
+                audio.pause();
+              }
+            }}
+          >
+            <SiApplemusic className="text-2xl" />
+          </button>
+        </ToolTip>
       </footer>
     </div>
   );
